efcbc4775b890490b91450700c849c275889e8d5
Ales Kotnik <ales.kotnik@nicehash.com>
2018-09-21 10:59:01 +0200

    Add deployment fixes

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 # build images
-docker build -t aleskotnik/multi-client -f ./client/Dockerfile ./client
-docker build -t aleskotnik/multi-server -f ./server/Dockerfile ./server
-docker build -t aleskotnik/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t aleskotnik/multi-client:latest -t aleskotnik/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t aleskotnik/multi-server:latest -t aleskotnik/multi-client:$SHA -f ./server/Dockerfile ./server
+docker build -t aleskotnik/multi-worker:latest -t aleskotnik/multi-client:$SHA -f ./worker/Dockerfile ./worker
 
 # push to docker hub
-docekr push aleskotnik/multi-client
-docekr push aleskotnik/multi-server
-docekr push aleskotnik/multi-worker
+docekr push aleskotnik/multi-client:latest
+docekr push aleskotnik/multi-client:$SHA
+docekr push aleskotnik/multi-server:latest
+docekr push aleskotnik/multi-server:$SHA
+docekr push aleskotnik/multi-worker:latest
+docekr push aleskotnik/multi-worker:$SHA
 
 # k8s
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment server=aleskotnik/multi-client
+kubectl set image deployments/client-deployment client=aleskotnik/multi-client:$SHA
+kubectl set image deployments/server-deployment server=aleskotnik/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=aleskotnik/multi-worker:$SHA
 
