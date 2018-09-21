0ade1f2ec5392b021eb11359b69cd1da84239d78
Ales Kotnik <ales.kotnik@nicehash.com>
2018-09-21 11:23:50 +0200

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
-#!/bin/sh
-
 # build images
 docker build -t aleskotnik/multi-client:latest -t aleskotnik/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t aleskotnik/multi-server:latest -t aleskotnik/multi-client:$SHA -f ./server/Dockerfile ./server
-docker build -t aleskotnik/multi-worker:latest -t aleskotnik/multi-client:$SHA -f ./worker/Dockerfile ./worker
+docker build -t aleskotnik/multi-server:latest -t aleskotnik/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t aleskotnik/multi-worker:latest -t aleskotnik/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 # push to docker hub
-docekr push aleskotnik/multi-client:latest
-docekr push aleskotnik/multi-client:$SHA
-docekr push aleskotnik/multi-server:latest
-docekr push aleskotnik/multi-server:$SHA
-docekr push aleskotnik/multi-worker:latest
-docekr push aleskotnik/multi-worker:$SHA
+docker push aleskotnik/multi-client:latest
+docker push aleskotnik/multi-client:$SHA
+docker push aleskotnik/multi-server:latest
+docker push aleskotnik/multi-server:$SHA
+docker push aleskotnik/multi-worker:latest
+docker push aleskotnik/multi-worker:$SHA
 
 # k8s
 kubectl apply -f k8s
